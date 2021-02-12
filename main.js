dec1bfc58892dbda95829ae5da1930cf85f57454
Cressens <Romain.Cressens@ugitech.com>
2021-02-12 19:41:20 +0100

    prepare to webpack + clean old html suff

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 import { basicFunctions } from './basic_alogirthm_scripting'
 import { intermediateFunctions } from './intermediate_algorithm_scripting'
-import interfaces from './interfaces.js'
 
-let calculateButton = document.getElementById('calculate')
-calculateButton.addEventListener('click', () => {
-
-    let sumDigit = null
-    try {
-        let input = document.getElementById('input')
-        sumDigit = functions.sum_items(input.value)
-    }
-    catch (error) {
-        interfaces.show_error(error)
-    }
-
-    interfaces.set_result(sumDigit)
-})
-
-console.log(basicFunctions.reverse_string("hello world"))
-
-console.log(intermediateFunctions.spinalCaseSplit("HelloWorld"))
+document.open();
+document.write("<p> reverse_string : " + basicFunctions.reverse_string("hello world") + "</p>");
+document.write("<p>spinalCaseSplit : " + intermediateFunctions.spinalCaseSplit("hello world") + "</p>");
+document.close();
